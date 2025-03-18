221319e81a03d4c1c2eb6c287dc00d7b1d7e7117
Daniele Spaccapeli <dspaccapeli@users.noreply.github.com>
2025-03-18 17:46:30 +0100

    Initial Commit

---
+++ Focus TotemTests/Focus_TotemTests.swift
@@ -0,0 +1,17 @@
+//
+//  Focus_TotemTests.swift
+//  Focus TotemTests
+//
+//  Created by Daniele Spaccapeli on 18/03/25.
+//
+
+import Testing
+@testable import Focus_Totem
+
+struct Focus_TotemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
